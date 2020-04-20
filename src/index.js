23d7877803ef4c1ea4dcafddba13b8b492576423
Cristian Morataya <cris.morataya@gmail.com>
2020-04-20 05:39:56 +0000

    Extracting components for reusability and readability

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 // v1 STAR MATCH - Starting Template
 
+const StarsDisplay = props => (
+    <>
+      {utils.range(1, props.count).map( starId =>
+          <div key={starId} className="star"/>
+      )}
+    </>
+)
+
+const PlayNumber = props => (
+    <button className="number" onClick={() => console.log('CM:: number', props.number)}>{props.number}</button>
+)
+
 const StarMatch = () => {
   const [stars, setStars] = useState(utils.random(1, 9));
   return (
@@ -13,13 +25,11 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-          {utils.range(1, stars).map( starId =>
-              <div key={starId} className="star"/>
-          )}
+          <StarsDisplay count={stars}/>
         </div>
         <div className="right">
           {utils.range(1, 9).map(number =>
-              <button key={number} className="number">{number}</button>
+              <PlayNumber key={number} number={number}/>
           )}
         </div>
       </div>
